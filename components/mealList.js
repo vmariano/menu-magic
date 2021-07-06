38ec52645e5199becb29f2fab37f5474eaf9a550
Don Vicente <m.vicent@gmail.com>
2021-07-06 12:13:30 -0300

    Setup meal list of 5 days.

--- components/mealList.js
+++ components/mealList.js
@@ -1,12 +1,21 @@
 import Meal from "./meal";
 
+const randomizeMenu = (list) => {
+    let sampleList = [];
+    let daysOfTheWeek = 5;
+    for (let i = 0; i < daysOfTheWeek; i++) {
+        sampleList.push(list[Math.floor(Math.random() * list.length)]);
+    }
+    return sampleList.map((meal) => {
+        return (<Meal key={meal.id}>{meal.content}</Meal>);
+    });
+}
+
 export default function mealList({title, list}) {
     return (
         <div>
             <h1>{title}</h1>
-            {list.map((meal) => {
-                return (<Meal key={meal.id}>{meal.content}</Meal>);
-            })}
+            {randomizeMenu(list)}
         </div>
     );
 }
