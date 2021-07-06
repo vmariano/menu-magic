7049583598b7ee79a808c714295a067df7c26e21
Don Vicente <m.vicent@gmail.com>
2021-07-06 13:15:25 -0300

    Add random menu on click.

--- components/meal.js
+++ components/meal.js
@@ -1,11 +1,17 @@
 import styles from '../styles/components/meal.module.css'
+import {useState} from "react";
 
-export default function Meal({children}) {
+
+export default function Meal({children, list}) {
+    const [content, setChildren] = useState(children);
     return(
         <div className={styles.mealContainer} >
             <div className={styles.draggablePattern}>⠀</div>
-            <p className={styles.mealName}>{children}</p>
-            <button>🎲</button>
+            <p className={styles.mealName}>{content}</p>
+            <button onClick={() =>  {
+                let sample = list[Math.floor(Math.random() * list.length)];
+                setChildren(sample.content)
+            }}>🎲</button>
         </div>
     );
 }
